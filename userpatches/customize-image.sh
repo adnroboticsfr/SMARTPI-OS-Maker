5a8a3b60f5fdfe9c279fedbef049370972e9cb58
adnroboticsfr <adnroboticsfr@gmail.com>
2024-10-05 13:23:48 +0200

    Add Files

---
+++ userpatches/customize-image.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# arguments: $RELEASE $LINUXFAMILY $BOARD $BUILD_DESKTOP
+#
+# This is the image customization script
+
+# NOTE: It is copied to /tmp directory inside the image
+# and executed there inside chroot environment
+# so don't reference any files that are not already installed
+
+# NOTE: If you want to transfer files between chroot and host
+# userpatches/overlay directory on host is bind-mounted to /tmp/overlay in chroot
+# The sd card's root path is accessible via $SDCARD variable.
+
+# shellcheck enable=requires-variable-braces
+# shellcheck disable=SC2034
+
+RELEASE=$1
+LINUXFAMILY=$2
+BOARD=$3
+BUILD_DESKTOP=$4
+
+Main() {
+    case "${BOARD}" in
+        smartpad)
+            rotateConsole
+            rotateScreen
+            rotateTouch
+            disableDPMS
+            if [[ "${BUILD_DESKTOP}" = "yes" ]]; then
+                patchLightdm
+                copyOnboardConf
+                patchOnboardAutostart
+                installScreensaverSetup
+            fi
+            ;;
+    esac
+}
+
+rotateConsole() {
+    local bootcfg
+    bootcfg="/boot/armbianEnv.txt"
+    echo "Rotate tty console by default ..."
+    echo "extraargs=fbcon=rotate:2" >> "${bootcfg}"
+    echo "Current configuration (${bootcfg}):"
+    cat "${bootcfg}"
+    echo "Rotate tty console by default ... done!"
+}
+
+rotateScreen() {
+    src="/tmp/overlay/02-smartpad-rotate-screen.conf"
+    dest="/etc/X11/xorg.conf.d/"
+    echo "Install rotated screen configuration ..."
+    cp -v "${src}" "${dest}"
+    echo "DEBUG:"
+    ls -l "${dest}"
+    echo "Install rotated screen configuration ... [DONE]"
+}
+
+rotateTouch() {
+    src="/tmp/overlay/03-smartpad-rotate-touch.conf"
+    dest="/etc/X11/xorg.conf.d/"
+    echo "Install rotated touch configuration ..."
+    cp -v "${src}" "${dest}"
+    echo "DEBUG:"
+    ls -l "${dest}"
+    echo "Install rotated touch configuration ... [DONE]"
+}
+
+disableDPMS() {
+    src="/tmp/overlay/04-smartpad-disable-dpms.conf"
+    dest="/etc/X11/xorg.conf.d/"
+    echo "Install rotated touch configuration ..."
+    cp -v "${src}" "${dest}"
+    echo "DEBUG:"
+    ls -l "${dest}"
+    echo "Install rotated touch configuration ... [DONE]"
+}
+
+patchLightdm() {
+    local conf
+    conf="/etc/lightdm/lightdm.conf.d/12-onboard.conf"
+    echo "Enable OnScreen Keyboard in Lightdm ..."
+    echo "onscreen-keyboard = true" | tee "${conf}"
+    echo "Enable OnScreen Keyboard in Lightdm ... [DONE]"
+}
+
+copyOnboardConf() {
+    echo "Copy onboard default configuration ..."
+    mkdir -p /etc/onboard
+    cp -v /tmp/overlay/onboard-defaults.conf /etc/onboard/
+    echo "Copy onboard default configuration ... [DONE]"
+}
+
+patchOnboardAutostart() {
+    local conf
+    conf="/etc/xdg/autostart/onboard-autostart.desktop"
+    echo "Patch Onboard Autostart file ..."
+    sed -i '/OnlyShowIn/s/^/# /' "${conf}"
+    echo "Patch Onboard Autostart file ... [DONE]"
+}
+
+installScreensaverSetup() {
+    src="/tmp/overlay/skel-xscreensaver"
+    dest="/etc/skel/.xscreensaver"
+    echo "Install rotated touch configuration ..."
+    \cp -fv "${src}" "${dest}"
+    echo "DEBUG:"
+    ls -al "$(dirname ${dest})"
+    echo "Install rotated touch configuration ... [DONE]"
+}
+
+Main "S{@}"
